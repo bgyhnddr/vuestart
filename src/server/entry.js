2e15e9e38587ca79333f3186b36a7112d3d99c5c
guanyi.bei <guanyi.bei@cyphertech.cn>
2016-10-26 12:38:38 +0800

    commit Signed-off-by: guanyi.bei <guanyi.bei@cyphertech.cn>

--- src/server/entry.js
+++ src/server/entry.js
@@ -22,33 +22,7 @@ module.exports = (app) => {
         if (req.params.permission == "private") {
             var checkPermission = require('../permission/check-permission')
             checkPermission(req, res, next).then(function() {
-                switch (req.params.type) {
-                    case "RBAC":
-                        require('./RBAC')(req, res, next)
-                        break
-                    case "upload":
-                        require('./upload')(req, res, next)
-                        break
-                    case "datasource":
-                        require('./datasource')(req, res, next)
-                        break
-                    case "create_quotation":
-                        require('./create_quotation')(req, res, next)
-                        break
-                    case "view_quotation":
-                        require('./view_quotation')(req, res, next)
-                        break
-                    case "edit_quotation":
-                        require('./edit_quotation')(req, res, next)
-                        break
-                    case "confirm_quotation":
-                        require('./confirm_quotation')(req, res, next)
-                        break
-                    case "confirm_quotation_boss":
-                        require('./confirm_quotation_boss')(req, res, next)
-                        break
-
-                }
+                require('./' + req.params.type)(req, res, next)
             }, function(error) {
                 if (error == "not_login") {
                     res.status(404).send({
@@ -63,11 +37,7 @@ module.exports = (app) => {
                 }
             })
         } else if (req.params.permission == "public") {
-            switch (req.params.type) {
-                case "auth":
-                    require('./auth')(req, res, next)
-                    break
-            }
+            require('./' + req.params.type)(req, res, next)
         }
     })
 }
